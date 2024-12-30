fac9e2022a3632a9d56252e0dec1a9eebac93954
Thomas Jon Barrientos <111177053+TjDev-Bot@users.noreply.github.com>
2024-12-30 13:45:23 +0800

    Pos master (#49)
    * fix seeders
    
    * add error handling sending email
    
    * modify Request Order
    
    * fix email sent
    
    * fix quick add for production
    
    * fix seeder for production
    
    * fix cardknox terminal
    
    * app_name
    
    ---------
    
    Co-authored-by: tejejeytejey13 <tejejeytejey13@gmail.com>

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>WebDaVinci Flow</title>
+    <title> {{ env('APP_NAME') }}</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -19,7 +19,7 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="#">WebDaVinci Flow</a>
+            <a href="#"> {{ env('APP_NAME') }} </a>
         </div>
         <!-- /.login-logo -->
         <div class="card">
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,6 +2,6 @@
     <div class="float-right d-none d-sm-block">
 
     </div>
-    <strong>&copy; <?php echo date('Y'); ?> - WebDaVinci Flow</strong>
+    <strong>&copy; <?php echo date('Y'); ?> -  {{ env('APP_NAME') }}</strong>
 </footer>
 
