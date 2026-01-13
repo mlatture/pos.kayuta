6af431656f3668a5f10ac2418fedaf6e32ba1cb0
malikahfaz <malikahfaz123@gmail.com>
2026-01-13 00:57:56 -0800

    working on step1 refactor changes

--- routes/web.php
+++ routes/web.php
@@ -161,6 +161,7 @@ Route::prefix('admin')
             Route::post('/update-customer/{draft_id}', [\App\Http\Controllers\FlowReservationController::class, 'updateCustomer'])->name('flow-reservation.update-customer');
             Route::post('/remove-item/{draft_id}', [\App\Http\Controllers\FlowReservationController::class, 'removeItem'])->name('flow-reservation.remove-item');
             Route::post('/finalize/{draft_id}', [\App\Http\Controllers\FlowReservationController::class, 'finalize'])->name('flow-reservation.finalize');
+            Route::get('/site-details', [\App\Http\Controllers\FlowReservationController::class, 'viewSiteDetails'])->name('flow-reservation.site-details');
         });
 
         Route::prefix('customers/{customer}/account')->group(function () {
